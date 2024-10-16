85fbb94d3119edb5ff6b0cadc215dfb2ef22c04d
LASYAPRIYABHAVANCHIKAR <lb68253n@pace.edu>
2024-10-15 21:36:07 -0400

    Got 9 squares

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -3,7 +3,14 @@ export default function Square() {
     <>
       <button className="square">X</button>
       <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+
     </>
   );
 }
-
